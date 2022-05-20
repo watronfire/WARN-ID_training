071d4744907d141c11152e6a53bb0863d4410ed9
watronfire <snowboardman007@gmail.com>
2022-05-20 14:33:52 -0700

    Use different pipeline for counts

--- software/tests/test_counts_pipeline.sh
+++ software/tests/test_counts_pipeline.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-for SAMPLE in CTRL2-dil-500 CTRL48-dil-1000 CTRL48 CTRL2 CTRL23 CTRL14 
-do
-	miniwdl run \
-		--dir /home/SeqLaptop/sequencing/output/sample_run/align_and_count_${SAMPLE}/. \
-		viral-pipelines/pipes/WDL/workflows/align_and_count.wdl \
-		align_and_count.reads_bam=/home/SeqLaptop/sequencing/output/sample_run/fastq_to_ubam_${SAMPLE}/call-FastqToUBAM/work/${SAMPLE}.bam \
-		align_and_count.ref_db=/home/SeqLaptop/sequencing/resources/sdsi_spike-ins_2021-08-09_update.fasta
-done
+miniwdl run \
+	--dir /home/seqlaptop/sequencing/output/sample_run/align_and_count_multiple_report/. \
+	viral-pipelines/pipes/WDL/workflows/align_and_count_multiple_report.wdl \
+	reads_unmapped_bams=/home/seqlaptop/sequencing/output/sample_run/fastq_to_ubam_CTRL2-dil-500/call-FastqToUBAM/work/CTRL2-dil-500.bam \
+	reads_unmapped_bams=/home/seqlaptop/sequencing/output/sample_run/fastq_to_ubam_CTRL48-dil-1000/call-FastqToUBAM/work/CTRL48-dil-1000.bam \
+	reads_unmapped_bams=/home/seqlaptop/sequencing/output/sample_run/fastq_to_ubam_CTRL48/call-FastqToUBAM/work/CTRL48.bam \
+	reads_unmapped_bams=/home/seqlaptop/sequencing/output/sample_run/fastq_to_ubam_CTRL2/call-FastqToUBAM/work/CTRL2.bam \
+	reads_unmapped_bams=/home/seqlaptop/sequencing/output/sample_run/fastq_to_ubam_CTRL23/call-FastqToUBAM/work/CTRL23.bam \
+	reads_unmapped_bams=/home/seqlaptop/sequencing/output/sample_run/fastq_to_ubam_CTRL14/call-FastqToUBAM/work/CTRL14.bam \
+	ref_db=/home/seqlaptop/sequencing/resources/sdsi_spike-ins_2021-08-09_update.fasta
